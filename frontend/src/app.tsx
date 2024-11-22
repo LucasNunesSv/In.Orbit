87de9f1b293132f5e48ed99dacd223a58235bd89
Lucas Nunes <lucasnsilveira202@gmail.com>
2024-11-21 22:39:07 -0300

    data fetch implement

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,14 +1,26 @@
 import { Dialog } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
 import { Summary } from "./components/summary";
+import { useEffect, useState } from "react";
 // import { EmptyGoal } from "./components/empty-goal";
 
 export function App() {
+
+  const [summary, setSummary] = useState()
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary').then(response => {
+      return response.json()
+    }).then(data => {
+      setSummary(data)
+    })
+  }, [])
+
   return (
     <Dialog>
       
       {/* <EmptyGoal /> */}
-
+      <pre>{JSON.stringify(summary, null, 2)}</pre>
       <Summary />
 
       <CreateGoal />
