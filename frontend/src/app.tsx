71ca8f70c0f86c6f2665bd3860779e915b2e944a
Lucas Nunes <lucasnsilveira202@gmail.com>
2024-11-26 10:11:23 -0300

    fetching data usig UseEffect and backend fixies

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,29 +2,37 @@ import { Dialog } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
 import { Summary } from "./components/summary";
 import { useEffect, useState } from "react";
-// import { EmptyGoal } from "./components/empty-goal";
+import { EmptyGoal } from "./components/empty-goal";
+
+type SummaryResposne = {
+    completed: number;
+    total: number;
+    goalsPerDay: Record<string, {
+        id: string;
+        title: string;
+        completedAt: string;
+    }[]>;
+}
 
 export function App() {
-
-  const [summary, setSummary] = useState()
+  
+  const [summary, setSummary] = useState<SummaryResposne | null>(null);
 
   useEffect(() => {
-    fetch('http://localhost:3333/summary').then(response => {
-      return response.json()
-    }).then(data => {
-      setSummary(data)
-    })
-  }, [])
+    fetch("http://localhost:3333/summary")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setSummary(data.summary);
+      });
+  }, []);
 
   return (
     <Dialog>
-      
-      {/* <EmptyGoal /> */}
-      <pre>{JSON.stringify(summary, null, 2)}</pre>
-      <Summary />
+      {summary?.total && summary.total > 0 ? <Summary /> : <EmptyGoal />}
 
       <CreateGoal />
-
     </Dialog>
   );
 }
